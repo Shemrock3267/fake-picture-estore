b0e4a7c16996a4a4d06550ba6997c37cbc7e9be3
Roman Oliinyk <dev.oliinyk@gmail.com>
2021-08-26 18:20:48 +0300

    rendering images via map

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,9 +1,19 @@
-import React from "react"
+import React, { useContext} from "react"
+import Image from '../components/Image.js';
+import { CONTEXT } from '../components/storeContext.js';
+import { getClass} from '../utils/index.js'
 
 function Photos() {
+    const { allPhotos } = useContext(CONTEXT);
+    const GALLERY = allPhotos.map((photo,index) => {
+        return (
+            <Image key={photo.id} img={photo} className={getClass(index)}/>
+        )
+    })
+   
     return (
         <main className="photos">
-            <h1>Images go here</h1>
+            {GALLERY}
         </main>
     )
 }
